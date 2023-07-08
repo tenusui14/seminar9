839306b7e4a96ccbf9befa663d7d4a18c62c614d
Artem Tsytsylin <workhardgofock@gmail.com>
2023-07-09 02:10:51 +0300

    is watched

---
+++ firsttaskfromIvan/Program.cs
@@ -0,0 +1,12 @@
+﻿// Перевести десятичное число в двоичное представление
+
+int number = 13;
+
+void PrintBinaryView(int decNumber){
+
+if (decNumber <= 0) return;
+PrintBinaryView(decNumber/2);
+Console.Write(decNumber%2);
+}
+
+PrintBinaryView(number);
