ddb23f3eceb9325ad338edc68342b33663ef759f
Artem Tsytsylin <workhardgofock@gmail.com>
2023-07-09 04:20:02 +0300

    HW task 68 is done

---
+++ HWtask68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+// m = 2, n = 3 -> A(m,n) = 29
+
+
+int Input(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermannFunction(m - 1, 1);
+    return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+}
+int m = Input("Введите m: ");
+int n = Input("Введите n: ");
+Console.WriteLine($"A(m,n) = {AckermannFunction(m, n)}");
+
